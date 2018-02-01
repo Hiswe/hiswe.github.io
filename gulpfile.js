bfec5a2295ed4c108996866a66c9145948c3be8e
Hiswe <hiswehalya@gmail.com>
2018-02-01 20:28:38 +0700

    BUILD – move to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,85 @@
-'use strict';
+'use strict'
 
-var gulp        = require('gulp');
-var $           = require('gulp-load-plugins')();
-var del 				= require('del');
-var lazypipe 		= require('lazypipe');
-var args        = require('yargs').argv;
-var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+const gulp        = require( `gulp` )
+const $           = require( `gulp-load-plugins` )()
+const del 				= require( `del` )
+const lazypipe 		= require( `lazypipe` )
+const args        = require( `yargs` ).argv
+const browserSync = require( `browser-sync` ).create()
+const reload      = browserSync.reload
 
-var isDev       = args.prod !== true;
-var themeDir 		= 'themes/hiswe-theme';
+const isDev       = args.prod !== true
+const themeDir 		= `themes/hiswe-theme`
 
 function onError(err) {
-  $.util.beep();
-  if (err.annotated)      { $.util.log(err.annotated); }
-  else if (err.message)   { $.util.log(err.message); }
-  else                    { $.util.log(err); }
-  return this.emit('end');
+  $.util.beep()
+  if (err.annotated)      { $.util.log(err.annotated) }
+  else if (err.message)   { $.util.log(err.message) }
+  else                    { $.util.log(err) }
+  return this.emit( `end` )
 }
 
 ////////
 // CSS
 ////////
 
-var autoprefixer  = require('autoprefixer');
-var cssDest 			= themeDir + '/source/css'
+const autoprefixer  = require('autoprefixer')
+const cssDest 			= `${ themeDir }/source/css`
 
-var cssDev        = lazypipe()
-  .pipe($.sourcemaps.write);
+const cssDev        = lazypipe()
+  .pipe( $.sourcemaps.write )
 
-var cssProd       = lazypipe()
-  .pipe($.minifyCss)
+const cssProd       = lazypipe()
+  .pipe( $.minifyCss )
 
-gulp.task('clean-css', function (cb) {
-  if (isDev) return cb();
-  return del([themeDir + '/*.css', cssDest + '/*.css.map'], cb);
-});
+function cleanCSS() {
+  if (isDev) return Promise.resolve()
+  return del([`${themeDir}/*.css`,`${cssDest}/*.css.map`])
+}
 
-gulp.task('css', ['clean-css'], function () {
+function compileCSS() {
   return gulp
-  	.src(themeDir + '/stylus/style.styl')
-    .pipe($.plumber(onError))
-    .pipe($.sourcemaps.init())
-    .pipe($.stylus())
-    .pipe($.postcss([
-        autoprefixer({ browsers: ['ie 10', 'last 2 versions'], }),
-      ]))
-    .pipe($.if(isDev, cssDev(), cssProd()))
-    .pipe(gulp.dest(cssDest))
-    .pipe($.if(isDev, reload({stream: true})));
-});
+  .src( `${themeDir}/stylus/style.styl` )
+  .pipe( $.plumber(onError) )
+  .pipe( $.sourcemaps.init() )
+  .pipe( $.stylus() )
+  .pipe( $.postcss([
+    autoprefixer({ browsers: ['ie 10', 'last 2 versions'], }),
+  ]) )
+  .pipe( $.if(isDev, cssDev(), cssProd()) )
+  .pipe( gulp.dest(cssDest) )
+  .pipe( $.if(isDev, reload({stream: true})) )
+}
+
+const css = gulp.series( cleanCSS, compileCSS )
 
 ////////
 // DEV
 ////////
 
-gulp.task('build', ['css']);
+const build = css
 
-gulp.task('dev', ['build'], function () {
-  browserSync.init({
-    proxy: 'http://localhost:4000',
-    open: false,
-    port: 7000,
-    ghostMode: false,
-  });
-  gulp.watch('source/**/**.{md, svg, png, jpg}', function () {
-    setTimeout(reload, 500);
-  });
-  gulp.watch(themeDir + '/stylus/**/*.styl',  ['css']);
-});
+function bs() {
+  return browserSync.init({
+    proxy:      `http://localhost:4000`,
+    open:       false,
+    port:       7000,
+    ghostMode:  false,
+  })
+}
 
-gulp.task('default', ['build']);
+function watch() {
+  gulp.watch( `source/**/**.{md,svg,png,jpg}`, () => setTimeout(reload, 500) )
+  gulp.watch( `${ themeDir }/stylus/**/*.styl`,  css )
+}
 
+const bsAndWatch = () => {
+  bs()
+  watch()
+}
 
+const dev = gulp.series(build, bsAndWatch)
 
+gulp.task( `dev`, dev )
+gulp.task( `build`, build )
+gulp.task( `css`, css )
